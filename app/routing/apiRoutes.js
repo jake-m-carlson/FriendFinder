8c532a9e77ee29791969b2f86a6f19efee91bdbc
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-10-06 22:32:23 -0500

    app.post added to api.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
     app.get('/api/friends', function (req, res) {
         res.json(friendData);
     });
+
+    app.post('/api/friends', function(req, res) {
+
+    });
 }
