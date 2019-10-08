81efb22d12de42ca769be4de69b82d92876fa7d2
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-10-08 17:23:11 -0500

    finished api.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,10 +3,34 @@ var friendData = require('../data/friends');
 module.exports = function (app) {
 
     app.get('/api/friends', function (req, res) {
-        res.json(friendData);
+        const friend = request.body;
+        const bestFriend = findBestFriend(friends, friend);
+        friendData.push(friend);
+        res.json(bestFriend);
     });
 
     app.post('/api/friends', function(req, res) {
         res.json(friendData);
     });
 };
+
+// get the best match
+function findBestFriend(friendData, friend) {
+    frineds.sort(function(f1, f2) {
+        const friend1ScoreDifference = compareScore(f1.scores, friend.scores);
+        const friend2ScoreDifference = compareScore(f2.scores, friend.scores);
+        return friend1ScoreDifference - friend2ScoreDifference;
+    });
+
+    return friendData[0];
+}
+
+// compare the scores
+function compareScore(score1, score2) {
+    let total = 0;
+    score1.forEach((score, i) => {
+        let diff = math.abs(score1[i] - score2[i]);
+        total = total + diff;
+    });
+    return total;
+}
