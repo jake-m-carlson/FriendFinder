853795c835f1318e314d80a71b2f775620db69a2
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-10-13 20:16:36 -0500

    debugged modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,35 +2,37 @@ var friendData = require("../data/friends");
 
 module.exports = function (app) {
 
-    app.get("./api/friends", function (req, res) {
-        const friend = request.body;
-        const bestFriend = findBestFriend(friends, friend);
+    app.get("/api/friends", function (req, res) {
+        const friend = req.body;
+        const bestFriend = findBestFriend(friendData, friend);
         friendData.push(friend);
         res.json(bestFriend);
     });
 
-    app.post("./api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         res.json(friendData);
     });
+
+
+    // get the best match
+    function findBestFriend(friendData, friend) {
+        friendData.sort(function (f1, f2) {
+            const friend1ScoreDifference = compareScore(f1.scores, friend.scores);
+            const friend2ScoreDifference = compareScore(f2.scores, friend.scores);
+            return friend1ScoreDifference - friend2ScoreDifference;
+        });
+
+        return friendData[0];
+    }
+
+    // compare the scores
+    function compareScore(score1, score2) {
+        let total = 0;
+        score1.forEach((score, i) => {
+            let diff = math.abs(score1[i] - score2[i]);
+            total = total + diff;
+        });
+        return total;
+    }
+
 };
-
-// get the best match
-function findBestFriend(friendData, friend) {
-    frineds.sort(function(f1, f2) {
-        const friend1ScoreDifference = compareScore(f1.scores, friend.scores);
-        const friend2ScoreDifference = compareScore(f2.scores, friend.scores);
-        return friend1ScoreDifference - friend2ScoreDifference;
-    });
-
-    return friendData[0];
-}
-
-// compare the scores
-function compareScore(score1, score2) {
-    let total = 0;
-    score1.forEach((score, i) => {
-        let diff = math.abs(score1[i] - score2[i]);
-        total = total + diff;
-    });
-    return total;
-}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@ var path = require('path');
 module.exports = function (app) {
 
     // viewed at http://localhost:8080
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
-});
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    });
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
-});
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
 
 };
